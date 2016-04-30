ed223f7d5c6f64d4ed23a0d39ac009ae61c93ac1
Josh Cain <jcain@redhat.com>
2016-04-30 09:34:30 -0500

    add token parse from base 64 string example

--- src/test/java/jcain/example/TokenParseTest.java
+++ src/test/java/jcain/example/TokenParseTest.java
@@ -1,5 +1,6 @@
 package jcain.example;
 
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
@@ -9,7 +10,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.security.KeyFactory;
 import java.security.KeyPair;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,10 +24,30 @@ public class TokenParseTest {
     public void parseSigAndValidateSubject() {
         final KeyPair keyPair = RsaProvider.generateKeyPair();
         final String token = Jwts.builder().setSubject("user1").signWith(SignatureAlgorithm.RS512, keyPair.getPrivate()).compact();
-        log.info("Generated JWT token: ", token);
+        log.info("Generated JWT token: {}", token);
 
         final Jws<Claims> parsedJwt = Jwts.parser().setSigningKey(keyPair.getPublic()).parseClaimsJws(token);
-        log.info("Successfully decrypted JWT ussing public key: " + parsedJwt.toString());
+        log.info("Successfully decrypted JWT using public key: {}", parsedJwt.toString());
         assertEquals("user1", parsedJwt.getBody().getSubject());
+    }
+
+    @Test
+    public void parseSigAndValidateFromPublicKeyString() throws Exception {
+        final KeyPair generateKeyPair = RsaProvider.generateKeyPair();
+        final String token = Jwts.builder().setSubject("user1").signWith(SignatureAlgorithm.RS512, generateKeyPair.getPrivate()).compact();
+        log.info("Generated JWT token: {}", token);
+
+        final String publicKeyString = Base64.encode(generateKeyPair.getPublic().getEncoded());
+
+        log.info("Parsing generated public key: {}", publicKeyString);
+        byte[] byteKey = Base64.decode(publicKeyString);
+        X509EncodedKeySpec X509publicKey = new X509EncodedKeySpec(byteKey);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        final PublicKey publicKey =  kf.generatePublic(X509publicKey);
+
+        final Jws<Claims> parsedJwt = Jwts.parser().setSigningKey(publicKey).parseClaimsJws(token);
+        log.info("Successfully decrypted JWT using parsed public key: {}", parsedJwt.toString());
+        assertEquals("user1", parsedJwt.getBody().getSubject());
+
     }
 }
